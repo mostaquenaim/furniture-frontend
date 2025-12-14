5b845ecef31beafd0f3d6a2080f1540c80ac4dfd
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-12-15 00:46:24 +0600

    delete auth callback

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,23 +1,11 @@
-"use client";
+import React from 'react';
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/context/AuthContext";
+const page = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
 
-export default function AuthCallback() {
-  const router = useRouter();
-  const login = useAuth();
-
-//   useEffect(() => {
-//     fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
-//       credentials: "include",
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         login(data);
-//         router.push("/");
-//       });
-//   }, []);
-
-  return <p>Signing you in...</p>;
-}
+export default page;
